82d6ef76d122b503561f62086948836ab5d08f4f
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2023-12-09 03:25:11 +0700

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -37,16 +37,6 @@ echo -e "$green.........................................................."$NC
 echo ''
 echo -e "$Lyellow                       Wait 6 Seconds!"$NC
 echo -e "$green.........................................................."$NC
-sleep 6
-clear
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
